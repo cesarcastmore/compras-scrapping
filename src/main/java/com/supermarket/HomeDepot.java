f0b96701eaaf26f32cb3a029c074f782a6a9a546
ccastillo_tegik <ccastillo@tegik.com>
2019-04-13 21:36:04 -0500

    esta trabajando home depot

--- src/main/java/com/supermarket/HomeDepot.java
+++ src/main/java/com/supermarket/HomeDepot.java
@@ -46,12 +46,13 @@ public class HomeDepot {
 
 		JSONArray listJson = new JSONArray();
 		String total ="0";
+		System.out.println("Entrando a home depot");
 
-  		String searchUrl = "https://www.homedepot.com.mx/SearchDisplay?searchTerm=" + searchQuery;
+  		String searchUrl = "https://www.homedepot.com.mx/SearchDisplay?categoryId=&storeId=10351&catalogId=10101&langId=-5&sType=SimpleSearch&resultCatEntryType=2&showResultsPage=true&searchSource=Q&pageView=&beginIndex=0&pageSize=80&searchTerm=" + searchQuery;
 
   		WebClient client = new WebClient(BrowserVersion.CHROME);  
-			client.getOptions().setCssEnabled(true);
-		client.getOptions().setJavaScriptEnabled(true);
+			client.getOptions().setCssEnabled(false);
+		client.getOptions().setJavaScriptEnabled(false);
 		client.getOptions().setThrowExceptionOnFailingStatusCode(false);
 		client.getOptions().setThrowExceptionOnScriptError(false);
 		client.setRefreshHandler(new ThreadedRefreshHandler());
@@ -62,10 +63,12 @@ public class HomeDepot {
 
 
   			page = client.getPage(searchUrl);
-  			Thread.sleep(5000);
+  			Thread.sleep(3000);
 
 
-  			System.out.println("HTML" +page.asXml());
+
+
+  			//System.out.println("HTML" +page.asXml());
 
 		} catch(Exception e){
   			e.printStackTrace();
@@ -73,7 +76,7 @@ public class HomeDepot {
 
 		
 		JSONObject res= new JSONObject();
-		addItems("div[@class='product']", listJson, page);
+		addItems("//ul[@class='grid_mode grid']/li", listJson, page);
 
 		res.put("results", listJson);
 		return res;
@@ -100,27 +103,39 @@ public class HomeDepot {
 		HtmlElement htmlItem = (HtmlElement) obj; 
 		JSONObject itemJson = new JSONObject();
 
-		System.out.println("HTML" +  htmlItem.asXml());
+		//System.out.println("HTML" +  htmlItem.asXml());
+		System.out.println("---------------------------------") ;
 
 
-  		HtmlElement imagenHtml = (HtmlElement) htmlItem.getFirstByXPath(".//img");  
-  		String imagen = imagenHtml.getAttribute("src");
+  		HtmlElement imagenHtml = (HtmlElement) htmlItem.getFirstByXPath(".//div[@class='image']/a/img");  
+  		//System.out.println("HTML" +  imagenHtml.asXml());
+
+  		String imagen = imagenHtml.getAttribute("data-src");
 		itemJson.put("imagen", imagen);
 
 
 
-  		HtmlElement linkHtml = (HtmlElement) htmlItem.getFirstByXPath(".//a");  
-  		String link = imagenHtml.getAttribute("href");
-  		String title = imagenHtml.getAttribute("title");
-		itemJson.put("enlace_informacion", link);
+  		HtmlElement linkHtml = (HtmlElement) htmlItem.getFirstByXPath(".//div[@class='product_name']/a");  
+  				System.out.println("HTML" +  linkHtml.asXml());
+
+  		String link = linkHtml.getAttribute("href");
+  		String title = linkHtml.asText();
+		itemJson.put("enlace_informacion", PAGE + link);
 		itemJson.put("titulo", title);
+		System.out.println(title);
+		
 
 		HtmlElement skuHtml = (HtmlElement) htmlItem.getFirstByXPath(".//span[@class='sku']");  
-		String sku= skuHtml.asText();
-		itemJson.put("sku", sku);
+		if(skuHtml != null){
+			String sku= skuHtml.asText();
+			itemJson.put("sku", sku);
 
-		HtmlElement priceHtml = (HtmlElement) htmlItem.getFirstByXPath(".//span[@itemprop='price']");  
-		String price= skuHtml.asText();
+		}
+
+		HtmlElement priceHtml = (HtmlElement) htmlItem.getFirstByXPath(".//span[@class='price']");  
+		String price= priceHtml.asText();
+		price= price.replace("$", "").trim();
+		price= price.substring(0, price.length() - 2);
 		itemJson.put("precio", price);
 
 
